df11485dcb897ab788d95a372b975485fd3b722e
AroniasPrenovost <Aroniascoder@gmail.com>
2016-09-19 23:46:13 -0700

    tweaked sketch.js, tested colors and settings

--- sketch.js
+++ sketch.js
@@ -3,16 +3,19 @@ function setup() {
 var canvas = createCanvas(585, 440);
 // this places my canvas inside a div element
 canvas.parent("sketch-holder");
-background(10, 182, 180);
+background(40, 40, 34);
 smooth();
-strokeWeight(.5);
+strokeWeight(.2);
 frameRate(60);
+
 }
 
 function draw() {
 mY = mouseY;  
 mX = mouseX;
-stroke((900));
+
+stroke((300));
 line(random(mouseX), random(mouseX), 280, 170);
+fill(72, 103, 184);
 }
 
